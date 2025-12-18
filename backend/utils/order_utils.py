1a4182549c6de33b8c4ca0fd8d2a5e5d73cc0188
ngodo0704@gmail.com <ngodo0704@gmail.com>
2025-12-18 14:13:28 +0700

    Them ham

--- backend/utils/order_utils.py
+++ backend/utils/order_utils.py
@@ -1,5 +1,5 @@
-from backend.models import Order
-
+from backend.models import Order, Session, SessionStatus, ProductOrder
+from backend import db
 
 def get_order_price(order_id):
     order = Order.query.get(order_id)
@@ -9,3 +9,23 @@ def get_order_price(order_id):
             total_price += detail.amount * detail.price_at_time
         return total_price
     return 0
+
+def get_verify_session(user_id):
+    sessionn = Session.query.filter(
+        Session.user_id == user_id,
+        Session.session_status == SessionStatus.ACTIVE
+    ).first()
+
+    return sessionn
+
+def add_order(order, ord):
+    if order:
+        for o in order.values():
+            r = ProductOrder(
+                order_id = ord.id,
+                product_id = o['id'],
+                amount = o['quantity'],
+                price_at_time = o['price']
+            )
+            db.session.execute(r)
+        db.session.commit()
